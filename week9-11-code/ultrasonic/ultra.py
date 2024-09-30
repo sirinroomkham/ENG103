327b311b9937f05ca47ef1486a421028be0bb93c
sirinroomkham <s.roomkham@gmail.com>
2024-09-30 16:57:51 +1000

    update warning sign for distance detection

--- week9-11-code/ultrasonic/ultra.py
+++ week9-11-code/ultrasonic/ultra.py
@@ -1,9 +1,12 @@
 from gpiozero import DistanceSensor
 from time import sleep
+#By switching to the pigpio pin factory, you'll get more accurate distance measurements from your ultrasonic sensor and avoid the PWM software fallback warning.
+from gpiozero.pins.pigpio import PiGPIOFactory
 
+factory = PiGPIOFactory() # prevening a warning erros
 # Initialize the DistanceSensor using GPIO Zero library
 # Trigger pin is connected to GPIO 4, Echo pin to GPIO 17
-sensor = DistanceSensor(echo=17, trigger=4, max_distance = 2)
+sensor = DistanceSensor(echo=17, trigger=4, max_distance = 2, pin_factory=factory)
 
 try:
     # Main loop to continuously measure and report distance
